7c5bb2b127362543fe96822a2269b42a08c4e2fc
Bushraabir <muzahidulislamabir123@gmail.com>
2026-02-04 12:21:37 +0600

    linear regression

--- 02.code-implementation/01_Linear_Regression.py
+++ 02.code-implementation/01_Linear_Regression.py
@@ -4,3 +4,10 @@ import seaborn as sns
 
 df = pd.read_csv('datasets/Ecommerce Customers')
 print(df.head()) # Display the first few rows of the dataframe
+
+df.info() # Get information about the dataframe
+print(df.describe()) # Get statistical summary of the dataframe
+
+
+
+
