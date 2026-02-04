a0519ae5ced42cc03d53fb0dd0fbb097480b8367
Bushraabir <muzahidulislamabir123@gmail.com>
2026-02-04 12:14:00 +0600

    linear regression

---
+++ 02.code-implementation/01_Linear_Regression.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('datasets/Ecommerce Customers')
+print(df.head()) # Display the first few rows of the dataframe
